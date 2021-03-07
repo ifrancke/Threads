6606c3f763ff024c52a5a9c3374621f43d9b4428
ifrancke <ifrancke@uvm.edu>
2021-03-06 19:25:39 -0500

    finished product

--- pidigits.ifrancke.c
+++ pidigits.ifrancke.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "pidigits.ifrancke.h"
-
+char buffer[BUFLEN];
 int readFile(char *fileName, int *numChars){
     FILE *fp = fopen(fileName, "r"); // filename is the char* passed to your readFile() function
+    
     if (fp == NULL) {
         printf("ERROR: cannot open file '%s'\n", fileName);
         return 1;
@@ -33,8 +35,31 @@ int readFile(char *fileName, int *numChars){
 
 }
 
+void *runner(void *param){
+    SearchStruct *data;
+    int longest = 0;
+    int count = 0;
+
+    data = (SearchStruct *) param;
+    for (int i=data->startIndex; i<=data->endIndex; ++i) {
+        if (buffer[i]-'0' == data->searchFor){
+            if(buffer[i+1]-'0' == data->searchFor){
+                ++count;
+            }
+            else{
+                if(count > longest){
+                    longest = count;
+                }
+                count = 1;
+            }
+        }
+  }
+  data->longestRun = longest;
+
+}
+
 int main(int argc, char *argv[]) {
-    int rc, numChars;
+    int rc, numChars, search;
     if (argc < 2) {
         printf("ERROR: need a filename\n");
         return 8;
@@ -42,9 +67,37 @@ int main(int argc, char *argv[]) {
     rc = readFile(argv[1], &numChars);
     if (rc != 0)
         return 8;
-    // here’s where you’ll create the threads that do the actual searching
-    for(int i = 0; i < nextFree; i++){
-        printf(buffer[i]);
+
+    printf("Enter the number you're looking for:\n");
+    scanf("%d", &search);
+
+    //setting up section separation
+    SearchStruct sections[NUMTHREADS];
+    pthread_t threads[NUMTHREADS];
+    for(int i = 0; i < NUMTHREADS; i++){
+        sections[i].searchFor = search;
+        //nextFree is the number of actual values in the buffer
+        sections[i].startIndex = i*nextFree/4;
+        sections[i].endIndex = (i+1)*nextFree/4;
+        if(i==3){
+            //need to decrement the last case so it isn't searching empty values
+            --sections[i].endIndex;
+        }
+        printf("I am thread %d. I start at %d and end at %d and I'm looking for %d.\n", i, sections[i].startIndex, sections[i].endIndex, sections[i].searchFor);
+        pthread_create(&threads[i], NULL, runner, &sections[i]);
     }
+    for(int i = 0; i < NUMTHREADS; i++){
+        pthread_join(threads[i], NULL);
+    }
+    int maxLongestRun = 0;
+    for(int i = 0; i < NUMTHREADS; i++){
+        printf("Longest run section %d : %d\n", i, sections[i].longestRun);
+        if(sections[i].longestRun > maxLongestRun){
+            maxLongestRun = sections[i].longestRun;
+        }
+    }
+
+    printf("Longest Run: %d\n", maxLongestRun);
+
     return 0;
 }
--- pidigits.ifrancke.h
+++ pidigits.ifrancke.h
@@ -1,7 +1,17 @@
 #define LINELEN 256
 #define BUFLEN 10100
-char buffer[BUFLEN];
+#define NUMTHREADS 4
+
+typedef struct {
+char searchFor; // character to search for
+int startIndex; // start index in buffer[] at which to begin searching
+int endIndex; // end index
+int longestRun; // result: the longest run found of the specified character in buffer[],
+// between startIndex and endIndex (inclusive)
+} SearchStruct;
+
 //nextFree is used to track the next available spot in the buffer
 int nextFree = 0;
 int readFile(char *fileName, int *numChars);
+void *runner(void *param);
 int main(int argc, char *argv[]);
